db3b5ac04cc66991f4945d01b11a977e95de6513
mark79-github <52877024+mark79-github@users.noreply.github.com>
2022-05-16 07:16:37 +0300

    Added JUnit tests for InvoiceService;

--- src/test/java/bg/softuni/invoice/service/RoleServiceTests.java
+++ src/test/java/bg/softuni/invoice/service/RoleServiceTests.java
@@ -25,7 +25,6 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-//@SpringBootTest
 class RoleServiceTests {
 
     private Role role;
