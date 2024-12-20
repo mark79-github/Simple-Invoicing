ac3bba09df437c262f9bd3a21b6e5bcd43af8929
mark79-github <52877024+mark79-github@users.noreply.github.com>
2024-12-20 20:19:54 +0200

    Add test for changing invoice statuses in ScheduleService

--- src/test/java/bg/softuni/invoice/service/impl/ScheduleServiceImplTest.java
+++ src/test/java/bg/softuni/invoice/service/impl/ScheduleServiceImplTest.java
@@ -1,5 +1,7 @@
 package bg.softuni.invoice.service.impl;
 
+import bg.softuni.invoice.model.enumerated.StatusType;
+import bg.softuni.invoice.model.service.InvoiceServiceModel;
 import bg.softuni.invoice.service.InvoiceService;
 import bg.softuni.invoice.service.LogService;
 import org.junit.jupiter.api.Test;
@@ -10,6 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.verify;
@@ -20,7 +23,6 @@ class ScheduleServiceImplTest {
     @MockBean
     private LogService logService;
 
-    @SuppressWarnings("unused")
     @MockBean
     private InvoiceService invoiceService;
 
@@ -38,5 +40,27 @@ class ScheduleServiceImplTest {
         LocalDateTime capturedDateTime = captor.getValue();
         assertTrue(capturedDateTime.isBefore(LocalDateTime.now().minusMinutes(4))
                 && capturedDateTime.isAfter(LocalDateTime.now().minusMinutes(6)));
+    }
+
+    @Test
+    void testChangeStatus_changesStatusOfInvoicesWithAwaitStatus() {
+        InvoiceServiceModel invoice1 = new InvoiceServiceModel();
+        invoice1.setId("1");
+        invoice1.setStatusType(StatusType.AWAIT);
+
+        InvoiceServiceModel invoice2 = new InvoiceServiceModel();
+        invoice2.setId("2");
+        invoice2.setStatusType(StatusType.AWAIT);
+
+        List<InvoiceServiceModel> mockInvoices = List.of(invoice1, invoice2);
+
+        Mockito.when(invoiceService.getAllInvoicesStatus(StatusType.AWAIT)).thenReturn(mockInvoices);
+
+        scheduleService.changeStatus();
+
+        Mockito.verify(invoiceService, Mockito.times(1)).getAllInvoicesStatus(StatusType.AWAIT);
+        Mockito.verify(invoiceService, Mockito.times(2)).changeStatus(Mockito.anyString());
+        Mockito.verify(invoiceService).changeStatus("1");
+        Mockito.verify(invoiceService).changeStatus("2");
     }
 }
