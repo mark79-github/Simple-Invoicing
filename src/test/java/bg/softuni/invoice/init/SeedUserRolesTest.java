69a661913e526fff686527ce9d788d2f6ebecf78
mark79-github <52877024+mark79-github@users.noreply.github.com>
2024-12-21 19:29:48 +0200

    Add unit tests for SeedUserRoles functionality

---
+++ src/test/java/bg/softuni/invoice/init/SeedUserRolesTest.java
@@ -0,0 +1,47 @@
+package bg.softuni.invoice.init;
+
+import bg.softuni.invoice.model.entity.Role;
+import bg.softuni.invoice.model.enumerated.RoleType;
+import bg.softuni.invoice.service.RoleService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.event.ContextRefreshedEvent;
+
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class SeedUserRolesTest {
+
+    @Autowired
+    private SeedUserRoles seedUserRoles;
+
+    @MockBean
+    private RoleService roleService;
+
+    @MockBean
+    private ContextRefreshedEvent contextRefreshedEvent;
+
+    @Test
+    void testSeedDoesNotAddRolesIfRolesAlreadyExist() {
+        when(roleService.getRoleRepositoryCount()).thenReturn(1L);
+
+        seedUserRoles.seed(contextRefreshedEvent);
+
+        verify(roleService, never()).addRole(any(Role.class));
+    }
+
+    @Test
+    void testSeedAddsRolesWhenDatabaseIsEmpty() {
+        when(roleService.getRoleRepositoryCount()).thenReturn(0L);
+
+        seedUserRoles.seed(contextRefreshedEvent);
+
+        verify(roleService, times(RoleType.values().length)).addRole(any(Role.class));
+    }
+}
