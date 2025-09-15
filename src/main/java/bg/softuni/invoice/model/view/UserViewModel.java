370d1b3ad3abf0309c410ca765fa66af294d8f59
mark79-github <52877024+mark79-github@users.noreply.github.com>
2025-09-15 08:38:28 +0300

    Refactor UserViewModel and UserServiceModel to extend UserBaseModel and remove duplicate fields

--- src/main/java/bg/softuni/invoice/model/view/UserViewModel.java
+++ src/main/java/bg/softuni/invoice/model/view/UserViewModel.java
@@ -1,47 +1,13 @@
 package bg.softuni.invoice.model.view;
 
+import bg.softuni.invoice.model.base.UserBaseModel;
+
+import java.util.HashSet;
 import java.util.Set;
 
-public class UserViewModel {
+public class UserViewModel extends UserBaseModel {
 
-    private String id;
-    private String username;
-    private String firstName;
-    private String lastName;
-    private Set<String> authorities;
-    private boolean enabled;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+    private Set<String> authorities = new HashSet<>();
 
     public Set<String> getAuthorities() {
         return authorities;
@@ -49,13 +15,5 @@ public class UserViewModel {
 
     public void setAuthorities(Set<String> authorities) {
         this.authorities = authorities;
-    }
-
-    public boolean isEnabled() {
-        return enabled;
-    }
-
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
     }
 }
