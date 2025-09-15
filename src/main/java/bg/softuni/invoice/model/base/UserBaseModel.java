ab368c2fb2179548d98f7cf6fe8dcfafcb9bf37a
mark79-github <52877024+mark79-github@users.noreply.github.com>
2025-09-15 08:38:04 +0300

    Add UserBaseModel abstract class

---
+++ src/main/java/bg/softuni/invoice/model/base/UserBaseModel.java
@@ -0,0 +1,50 @@
+package bg.softuni.invoice.model.base;
+
+public abstract class UserBaseModel {
+
+    private String id;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private boolean enabled;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+}
