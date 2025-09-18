651c80d64b2b91b6d83af20b497faf63f68ef4f7
mark79-github <52877024+mark79-github@users.noreply.github.com>
2025-09-18 06:10:12 +0300

    Annotate Role entity with Lombok annotations

--- src/main/java/bg/softuni/invoice/model/entity/Role.java
+++ src/main/java/bg/softuni/invoice/model/entity/Role.java
@@ -4,31 +4,27 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 
 import static bg.softuni.invoice.constant.ErrorMsg.AUTHORITY_NOT_EMPTY;
 
+@Setter
 @Entity
 @Table(name = "roles")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Role extends BaseEntity implements GrantedAuthority {
 
     @Column(name = "authority", nullable = false, updatable = false, unique = true)
     @NotEmpty(message = AUTHORITY_NOT_EMPTY)
     private String authority;
 
-    public Role() {
-    }
-
-    public Role(String authority) {
-        this.authority = authority;
-    }
-
     @Override
     public String getAuthority() {
         return this.authority;
     }
 
-    public void setAuthority(String authority) {
-        this.authority = authority;
-    }
 }
