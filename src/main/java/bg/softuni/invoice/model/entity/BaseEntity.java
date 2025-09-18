190f60c3115e9c2cab3f5d8d874fa865451f866b
mark79-github <52877024+mark79-github@users.noreply.github.com>
2025-09-18 06:08:23 +0300

    Annotate BaseEntity with Lombok annotations

--- src/main/java/bg/softuni/invoice/model/entity/BaseEntity.java
+++ src/main/java/bg/softuni/invoice/model/entity/BaseEntity.java
@@ -3,9 +3,17 @@ package bg.softuni.invoice.model.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.UuidGenerator;
 
+import static lombok.AccessLevel.PROTECTED;
+
+@Setter
+@Getter
 @MappedSuperclass
+@NoArgsConstructor(access = PROTECTED)
 public abstract class BaseEntity {
 
     @Id
@@ -13,14 +21,4 @@ public abstract class BaseEntity {
     @Column(nullable = false, unique = true, updatable = false)
     private String id;
 
-    protected BaseEntity() {
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
 }
