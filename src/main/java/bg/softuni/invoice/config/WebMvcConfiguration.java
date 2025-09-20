2fef76a478b9f35fc9ae71141f471d5e58d44c88
mark79-github <52877024+mark79-github@users.noreply.github.com>
2025-09-20 06:05:03 +0300

    Annotate WebMvcConfiguration with Lombok annotation

--- src/main/java/bg/softuni/invoice/config/WebMvcConfiguration.java
+++ src/main/java/bg/softuni/invoice/config/WebMvcConfiguration.java
@@ -3,24 +3,18 @@ package bg.softuni.invoice.config;
 import bg.softuni.invoice.web.interceptor.FavIconInterceptor;
 import bg.softuni.invoice.web.interceptor.LoggingInterceptor;
 import bg.softuni.invoice.web.interceptor.TitleInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@AllArgsConstructor
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
     private final TitleInterceptor titleInterceptor;
     private final FavIconInterceptor faviconInterceptor;
     private final LoggingInterceptor loggingInterceptor;
-
-    @Autowired
-    public WebMvcConfiguration(TitleInterceptor titleInterceptor, FavIconInterceptor faviconInterceptor, LoggingInterceptor loggingInterceptor) {
-        this.titleInterceptor = titleInterceptor;
-        this.faviconInterceptor = faviconInterceptor;
-        this.loggingInterceptor = loggingInterceptor;
-    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
