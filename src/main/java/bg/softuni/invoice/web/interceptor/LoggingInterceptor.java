0d6a100a8dbe4be2d541313baa13d4401e819885
mark79-github <52877024+mark79-github@users.noreply.github.com>
2025-09-21 09:21:26 +0300

    Annotate LoggingInterceptor with Lombok @RequiredArgsConstructor annotation

--- src/main/java/bg/softuni/invoice/web/interceptor/LoggingInterceptor.java
+++ src/main/java/bg/softuni/invoice/web/interceptor/LoggingInterceptor.java
@@ -4,34 +4,26 @@ import bg.softuni.invoice.model.service.LogServiceModel;
 import bg.softuni.invoice.model.service.UserServiceModel;
 import bg.softuni.invoice.service.LogService;
 import bg.softuni.invoice.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import java.time.LocalDateTime;
 
 import static bg.softuni.invoice.constant.GlobalConstants.ANONYMOUS_USER_USERNAME;
 
 @Component
+@RequiredArgsConstructor
 public class LoggingInterceptor implements HandlerInterceptor {
 
     private final LogService logService;
     private final UserService userService;
 
-    @Autowired
-    public LoggingInterceptor(LogService logService, UserService userService) {
-        this.logService = logService;
-        this.userService = userService;
-    }
-
     @Override
-    public boolean preHandle(HttpServletRequest request,
-                             HttpServletResponse response,
-                             Object handler) throws Exception {
-
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         String requestURI = request.getRequestURI();
         String method = request.getMethod();
         String username = request.getUserPrincipal() == null
