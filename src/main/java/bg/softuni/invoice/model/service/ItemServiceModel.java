0be09b738c8437171c7b67a4096aea0cb2d3bfc9
mark79-github <52877024+mark79-github@users.noreply.github.com>
2025-09-23 06:57:29 +0300

    Annotate ItemServiceModel with Lombok Getter and Setter annotations

--- src/main/java/bg/softuni/invoice/model/service/ItemServiceModel.java
+++ src/main/java/bg/softuni/invoice/model/service/ItemServiceModel.java
@@ -1,9 +1,13 @@
 package bg.softuni.invoice.model.service;
 
 import bg.softuni.invoice.model.enumerated.VatValue;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 
+@Setter
+@Getter
 public class ItemServiceModel {
 
     private String id;
@@ -13,51 +17,4 @@ public class ItemServiceModel {
     private String imageUrl;
     private int quantity;
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public VatValue getVatValue() {
-        return vatValue;
-    }
-
-    public void setVatValue(VatValue vatValue) {
-        this.vatValue = vatValue;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
 }
