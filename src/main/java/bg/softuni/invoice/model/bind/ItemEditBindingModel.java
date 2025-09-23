8f5c4cca9a2c9c824be3a54b414a94a2e5d71ac1
mark79-github <52877024+mark79-github@users.noreply.github.com>
2025-09-23 06:58:19 +0300

    Annotate ItemEditBindingModel with Lombok Getter and Setter annotations

--- src/main/java/bg/softuni/invoice/model/bind/ItemEditBindingModel.java
+++ src/main/java/bg/softuni/invoice/model/bind/ItemEditBindingModel.java
@@ -3,6 +3,8 @@ package bg.softuni.invoice.model.bind;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -13,6 +15,8 @@ import static bg.softuni.invoice.constant.ErrorMsg.NAME_MIN_LENGTH;
 import static bg.softuni.invoice.constant.ErrorMsg.PRICE_POSITIVE;
 import static bg.softuni.invoice.constant.ErrorMsg.STRING_MIN_LENGTH;
 
+@Setter
+@Getter
 public class ItemEditBindingModel {
 
     private String id;
@@ -33,51 +37,4 @@ public class ItemEditBindingModel {
 
     private MultipartFile newImageUrl;
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public String getVatValue() {
-        return vatValue;
-    }
-
-    public void setVatValue(String vatValue) {
-        this.vatValue = vatValue;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    public MultipartFile getNewImageUrl() {
-        return newImageUrl;
-    }
-
-    public void setNewImageUrl(MultipartFile newImageUrl) {
-        this.newImageUrl = newImageUrl;
-    }
 }
