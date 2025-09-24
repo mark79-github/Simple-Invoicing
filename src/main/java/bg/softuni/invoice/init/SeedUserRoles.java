0428c3052a5a7c36de6ce64763fe993b816a0317
mark79-github <52877024+mark79-github@users.noreply.github.com>
2025-09-24 06:19:04 +0300

    Annotate SeedUserRoles with Lombok RequiredArgsConstructor annotation

--- src/main/java/bg/softuni/invoice/init/SeedUserRoles.java
+++ src/main/java/bg/softuni/invoice/init/SeedUserRoles.java
@@ -3,7 +3,7 @@ package bg.softuni.invoice.init;
 import bg.softuni.invoice.model.entity.Role;
 import bg.softuni.invoice.model.enumerated.RoleType;
 import bg.softuni.invoice.service.RoleService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
@@ -11,17 +11,12 @@ import org.springframework.stereotype.Component;
 import java.util.Arrays;
 
 @Component
+@RequiredArgsConstructor
 public class SeedUserRoles {
-
-    private boolean seedInitialized = false;
 
     private final RoleService roleService;
 
-    @Autowired
-    public SeedUserRoles(RoleService roleService) {
-        this.roleService = roleService;
-    }
-
+    private boolean seedInitialized = false;
 
     @EventListener
     public void seed(ContextRefreshedEvent event) {
