1e593fbc5bcaa4b11c3514f331a95691dc466220
mark79-github <52877024+mark79-github@users.noreply.github.com>
2025-09-25 06:24:14 +0300

    Annotate SaleViewModel with Lombok Getter and Setter annotations

--- src/main/java/bg/softuni/invoice/model/view/SaleViewModel.java
+++ src/main/java/bg/softuni/invoice/model/view/SaleViewModel.java
@@ -1,7 +1,12 @@
 package bg.softuni.invoice.model.view;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 
+@Setter
+@Getter
 public class SaleViewModel {
 
     private String id;
@@ -10,43 +15,4 @@ public class SaleViewModel {
     private BigDecimal price;
     private int vatValue;
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public int getVatValue() {
-        return vatValue;
-    }
-
-    public void setVatValue(int vatValue) {
-        this.vatValue = vatValue;
-    }
 }
