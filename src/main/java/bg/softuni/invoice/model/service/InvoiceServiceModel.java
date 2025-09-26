6f6a3c30468544977e3ccd9769f4290363972e21
mark79-github <52877024+mark79-github@users.noreply.github.com>
2025-09-26 06:10:30 +0300

    Annotate InvoiceServiceModel with Lombok Getter and Setter annotations

--- src/main/java/bg/softuni/invoice/model/service/InvoiceServiceModel.java
+++ src/main/java/bg/softuni/invoice/model/service/InvoiceServiceModel.java
@@ -2,12 +2,16 @@ package bg.softuni.invoice.model.service;
 
 import bg.softuni.invoice.model.enumerated.PaymentType;
 import bg.softuni.invoice.model.enumerated.StatusType;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Set;
 
+@Setter
+@Getter
 public class InvoiceServiceModel {
 
     private String id;
@@ -22,91 +26,4 @@ public class InvoiceServiceModel {
     private LocalDateTime createdOn;
     private Set<SaleServiceModel> sales;
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public BigDecimal getTotalValue() {
-        return totalValue;
-    }
-
-    public void setTotalValue(BigDecimal totalValue) {
-        this.totalValue = totalValue;
-    }
-
-    public UserServiceModel getUser() {
-        return user;
-    }
-
-    public void setUser(UserServiceModel user) {
-        this.user = user;
-    }
-
-    public CompanyServiceModel getSender() {
-        return sender;
-    }
-
-    public void setSender(CompanyServiceModel sender) {
-        this.sender = sender;
-    }
-
-    public CompanyServiceModel getReceiver() {
-        return receiver;
-    }
-
-    public void setReceiver(CompanyServiceModel receiver) {
-        this.receiver = receiver;
-    }
-
-    public PaymentType getPaymentType() {
-        return paymentType;
-    }
-
-    public void setPaymentType(PaymentType paymentType) {
-        this.paymentType = paymentType;
-    }
-
-    public StatusType getStatusType() {
-        return statusType;
-    }
-
-    public void setStatusType(StatusType statusType) {
-        this.statusType = statusType;
-    }
-
-    public long getInvoiceNumber() {
-        return invoiceNumber;
-    }
-
-    public void setInvoiceNumber(long invoiceNumber) {
-        this.invoiceNumber = invoiceNumber;
-    }
-
-    public LocalDateTime getCreatedOn() {
-        return createdOn;
-    }
-
-    public void setCreatedOn(LocalDateTime createdOn) {
-        this.createdOn = createdOn;
-    }
-
-    public Set<SaleServiceModel> getSales() {
-        return sales;
-    }
-
-    public void setSales(Set<SaleServiceModel> sales) {
-        this.sales = sales;
-    }
 }
