a7b4a3aa1731839a9788d56561fabf118bf4c68c
mark79-github <52877024+mark79-github@users.noreply.github.com>
2025-09-27 06:03:25 +0300

    Annotate UserViewModel with Lombok Getter and Setter annotations

--- src/main/java/bg/softuni/invoice/model/view/UserViewModel.java
+++ src/main/java/bg/softuni/invoice/model/view/UserViewModel.java
@@ -1,61 +1,20 @@
 package bg.softuni.invoice.model.view;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
 public class UserViewModel {
 
     private String id;
     private String username;
     private String firstName;
     private String lastName;
-    private Set<String> authorities;
+    private Set<String> authorities = new HashSet<>();
     private boolean enabled;
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public Set<String> getAuthorities() {
-        return authorities;
-    }
-
-    public void setAuthorities(Set<String> authorities) {
-        this.authorities = authorities;
-    }
-
-    public boolean isEnabled() {
-        return enabled;
-    }
-
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
-    }
 }
