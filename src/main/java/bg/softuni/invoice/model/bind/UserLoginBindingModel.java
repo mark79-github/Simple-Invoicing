64ba78c8c226ddae7c6987d3602342c14d4f635f
mark79-github <52877024+mark79-github@users.noreply.github.com>
2025-09-29 05:55:54 +0300

    Extend UserLoginBindingModel to inherit from UserBaseBindingModel

--- src/main/java/bg/softuni/invoice/model/bind/UserLoginBindingModel.java
+++ src/main/java/bg/softuni/invoice/model/bind/UserLoginBindingModel.java
@@ -1,23 +1,14 @@
 package bg.softuni.invoice.model.bind;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
-import static bg.softuni.invoice.constant.ErrorMsg.EMAIL_NOT_CORRECT;
-import static bg.softuni.invoice.constant.ErrorMsg.EMAIL_NOT_EMPTY;
-import static bg.softuni.invoice.constant.ErrorMsg.EMAIL_REGEX;
 import static bg.softuni.invoice.constant.ErrorMsg.PASSWORD_NOT_EMPTY;
 
 @Setter
 @Getter
-public class UserLoginBindingModel {
-
-    @NotBlank(message = EMAIL_NOT_EMPTY)
-    @Pattern(regexp = EMAIL_REGEX,
-            message = EMAIL_NOT_CORRECT)
-    private String username;
+public class UserLoginBindingModel extends UserBaseBindingModel {
 
     @NotBlank(message = PASSWORD_NOT_EMPTY)
     private String password;
