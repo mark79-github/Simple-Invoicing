e7e3ef4840a934ec117216ef07a44e4fe22885d4
mark79-github <52877024+mark79-github@users.noreply.github.com>
2026-02-14 05:55:52 +0200

    Fix incorrect import to use correct DataJpaTest annotation

--- src/test/java/bg/softuni/invoice/repository/CompanyRepositoryTest.java
+++ src/test/java/bg/softuni/invoice/repository/CompanyRepositoryTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 
 import java.util.List;
 import java.util.Optional;
