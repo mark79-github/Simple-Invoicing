a3e2d2925cf48b6a34b6f3b0130425c2256869f5
mark79-github <52877024+mark79-github@users.noreply.github.com>
2026-02-14 06:01:40 +0200

    Fix incorrect import in LogRepositoryTest

--- src/test/java/bg/softuni/invoice/repository/LogRepositoryTest.java
+++ src/test/java/bg/softuni/invoice/repository/LogRepositoryTest.java
@@ -5,7 +5,7 @@ import bg.softuni.invoice.model.entity.User;
 import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 
 import java.time.LocalDateTime;
 import java.util.List;
