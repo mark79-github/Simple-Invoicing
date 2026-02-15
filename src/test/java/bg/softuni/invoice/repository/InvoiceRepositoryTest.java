35d3b1a3a48c3cee2e12bf74e1e46ddbc56c86e5
mark79-github <52877024+mark79-github@users.noreply.github.com>
2026-02-15 05:48:03 +0200

    Fix incorrect import in InvoiceRepositoryTest

--- src/test/java/bg/softuni/invoice/repository/InvoiceRepositoryTest.java
+++ src/test/java/bg/softuni/invoice/repository/InvoiceRepositoryTest.java
@@ -9,7 +9,7 @@ import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
