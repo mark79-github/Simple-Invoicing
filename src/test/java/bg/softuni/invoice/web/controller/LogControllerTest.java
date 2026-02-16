f1923cfb7795c42d632b00671e3f4edb5167755a
mark79-github <52877024+mark79-github@users.noreply.github.com>
2026-02-16 06:07:43 +0200

    Fix incorrect imports and adjust LogControllerTest to replace WithMockUser with MockMvc user setup

--- src/test/java/bg/softuni/invoice/web/controller/LogControllerTest.java
+++ src/test/java/bg/softuni/invoice/web/controller/LogControllerTest.java
@@ -1,16 +1,15 @@
-package bg.softuni.invoice.controller;
+package bg.softuni.invoice.web.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -20,10 +19,10 @@ class LogControllerTest {
     private MockMvc mockMvc;
 
     @Test
-    @WithMockUser(roles = {"ROOT"})
     void all_shouldReturnCorrectView() throws Exception {
         this.mockMvc
-                .perform(get("/log/all").with(csrf()))
+                .perform(get("/log/all").with(user("test-root").roles("ROOT")))
+                .andExpect(status().isOk())
                 .andExpect(view().name("log/all"));
     }
 }
